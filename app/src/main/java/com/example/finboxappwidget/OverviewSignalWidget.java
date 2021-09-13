9ee2f8f01ed74a7cff5257bd08c87f0227ec357c
vantan2705 <43321300+vantan2705@users.noreply.github.com>
2021-09-13 19:50:05 +0700

    Add chart signal

--- app/src/main/java/com/example/finboxappwidget/OverviewSignalWidget.java
+++ app/src/main/java/com/example/finboxappwidget/OverviewSignalWidget.java
@@ -3,18 +3,29 @@ package com.example.finboxappwidget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
+
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -23,7 +34,6 @@ import java.util.HashMap;
 public class OverviewSignalWidget extends AppWidgetProvider {
 
     private String dataUrl = "https://api.finbox.vn/api/app_new/getMarketData/";
-    private String imageUrl = "https://api.finbox.vn/api/widget/chart/signal";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -50,30 +60,26 @@ public class OverviewSignalWidget extends AppWidgetProvider {
                             String ratio = chartBaseJSON.getString("ratio");
                             String note = chartBaseJSON.getString("note");
 
-                            imageUrl += "?buy=" + buy + "&sell=" + sell;
-                            ImageLoader imageLoader = MySingleton.getInstance(context).getImageLoader();
-                            imageLoader.get(imageUrl, new ImageLoader.ImageListener() {
-                                @Override
-                                public void onResponse(ImageLoader.ImageContainer imageResponse, boolean isImmediate) {
+                            float fBuy;
+                            float fSell;
 
-                                    if(imageResponse != null && imageResponse.getBitmap() != null){
-                                        for (int i = 0; i<N; i++) {
-                                            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.overview_signal_widget);
-                                            views.setTextViewText(R.id.txtWidgetSignalNote, note);
-                                            views.setTextViewText(R.id.txtWidgetSignalBuy, buy);
-                                            views.setTextViewText(R.id.txtWidgetSignalSell, sell);
-                                            views.setTextViewText(R.id.txtWidgetSignalRatio, ratio);
-                                            views.setImageViewBitmap(R.id.imageViewWidgetSignal, imageResponse.getBitmap());
-                                            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
-                                        }
-                                    }
-                                }
+                            try {
+                                fBuy = Float.parseFloat(buy);
+                                fSell = Float.parseFloat(sell);
+                            } catch (Exception e) {
+                                fBuy = 0f;
+                                fSell = 0f;
+                            }
 
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.e("widget volley error", error.getMessage());
-                                }
-                            });
+                            for (int i = 0; i<N; i++) {
+                                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.overview_signal_widget);
+                                views.setTextViewText(R.id.txtWidgetSignalNote, note);
+                                views.setTextViewText(R.id.txtWidgetSignalBuy, buy);
+                                views.setTextViewText(R.id.txtWidgetSignalSell, sell);
+                                views.setTextViewText(R.id.txtWidgetSignalRatio, ratio);
+                                views.setImageViewBitmap(R.id.imageViewWidgetSignal, chart(context, fBuy, fSell));
+                                appWidgetManager.updateAppWidget(appWidgetIds[i], views);
+                            }
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -101,6 +107,60 @@ public class OverviewSignalWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public Bitmap chart(Context context, float buy, float sell) {
+        int positiveColor = ContextCompat.getColor(context, R.color.widget_overview_positive);
+        int negativeColor = ContextCompat.getColor(context, R.color.widget_overview_negative);
+        BarChart chart = new BarChart(context);
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, buy));
+        entries.add(new BarEntry(6f, sell));
+
+        BarDataSet dataset = new BarDataSet(entries, "");
+        dataset.setColors(new int[] {positiveColor, negativeColor});
+        dataset.setValueTextSize(25);
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Buy");
+        labels.add("Sell");
+        BarData data = new BarData(dataset);
+        data.setBarWidth(5f);
+        data.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return String.valueOf((int) value);
+            }
+        });
+        chart.setData(data);
+        chart.getDescription().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+        chart.setFitBars(true); // value align center
+        chart.setExtraTopOffset(50); // Top padding to avoid value covered
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setEnabled(false);
+        xAxis.setDrawGridLines(false);
+        chart.getAxisLeft().setAxisMinimum(0f); // Đúng tỉ lệ giữa 2 cột
+        chart.getAxisRight().setAxisMinimum(0f); // Đúng tỉ lệ giữa 2 cột
+        chart.getAxisLeft().setTextSize(15f);
+        chart.getAxisLeft().setDrawLabels(false);
+        chart.getAxisRight().setDrawLabels(false);
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getAxisRight().setDrawGridLines(false);
+
+        chart.setDrawingCacheEnabled(true);
+        // this is the important code :)
+        // Without it the view will have a dimension of 0,0 and the bitmap will be null
+        chart.measure(View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY));
+        chart.layout(0, 0, chart.getMeasuredWidth(), chart.getMeasuredHeight());
+        chart.buildDrawingCache(true);
+        Bitmap bitmap = Bitmap.createBitmap(chart.getDrawingCache());
+        chart.setDrawingCacheEnabled(false); // clear drawing cache
+        return bitmap;
     }
 
 }
